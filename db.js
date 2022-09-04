6936c134ec908cc58b8a0045398bb5feb1f04635
masud-orangetoolz <masud.rana@orangetoolz.com>
2022-09-04 21:45:31 +0600

    refactor: videos data add menually

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
-const todos = require("./data/todos");
-const videos = require("./data/videos");
+const todos = require("./data/todos.json");
+const videos = require("./data/videos.json");
+const tags = require("./data/tags");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
   videos,
+  tags,
   videos2,
   transactions,
 }
